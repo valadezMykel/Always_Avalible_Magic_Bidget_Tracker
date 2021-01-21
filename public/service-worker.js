ad15bfbb68dac4440b4ffdc2e68f24489ec0c1f5
Mykel Valadez <valadezmykel@gmail.com>
2021-01-21 13:14:27 -0800

    service worker is listening for fetches

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,30 @@
 const FILES_TO_CACHE = [
-    "/"
+    '/',
+    '/index.js',
+    '/styles.css'
 ]
-const CACHE_NAME = "static-cache"
-const DATA_CACHE_NAME = "data-cache"
+const CACHE_NAME = 'static-cache'
+const DATA_CACHE_NAME = 'data-cache'
+
+self.addEventListener('install', (event) => {
+    
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    )
+
+    self.skipWaiting()
+})
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if(response){
+                    console.log("returned from cache")
+                    return response
+                }
+
+                return fetch(event.request)
+            })
+    )
+})
